97e1d1bbcaf2d110061d5c4d4aaa3af21fce7343
Cecilia Perdomo <83676442+CeciliaBPerdomo@users.noreply.github.com>
2024-05-06 18:35:54 +0000

    To do list

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react'
 const TodoList = () => {
     const [list, setList] = useState("");
     const [toDo, setToDo] = useState([{}]);
+
     const crearUsuario = async () => {
         try {
             const response = await fetch("https://playground.4geeks.com/todo/users/aratarjat", {
@@ -29,30 +30,18 @@ const TodoList = () => {
         }
     }
 
-    const actualizarTareas = async () => {
-        try {
-            const response = await fetch("https://playground.4geeks.com/todo/users/aratarjat", {
-                method: "PUT",
-                body: JSON.stringify(toDo),
-                headers: { "Content-Type": "application/json" }
-            })
-            const data = await response.json()
-            console.log(data)
-
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
     const guardarTarea = async () => {
+        const tarea = {
+            label: list,
+            is_done: false
+        }
         try {
-            const response = await fetch("https://playground.4geeks.com/todo/users/aratarjat", {
-                method: "PUT",
-                body: JSON.stringify(toDo),
+            await fetch("https://playground.4geeks.com/todo/todos/aratarjat", {
+                method: "POST",
+                body: JSON.stringify(tarea),
                 headers: { "Content-Type": "application/json" }
             })
-            const data = await response.json()
-            console.log(data)
+            obtnenerTareas()
         } catch (error) {
             console.log(error)
         }
@@ -63,50 +52,59 @@ const TodoList = () => {
         obtnenerTareas()
     }, []) // se ejecuta una solo vez, posterior a cargar el componente. 
 
-    useEffect(() => {
-        actualizarTareas()
-    }, [toDo]) // no se ejecuta una solo vez, sino cada vez que haya un cambio en la lista de tareas. 
 
-    const funcionDeBorrado = (value) => {
-        const copiaToDo = [...toDo]
-        let arregloFiltrado = copiaToDo.filter((tarea) => tarea.value !== value)
-        console.log("este es tu arreglo despues de borrado", arregloFiltrado)
-        setToDo(arregloFiltrado)
+    const funcionDeBorrado = async (e, id) => {
+        e.preventDefault()
+        console.log(id)
+        try {
+            await fetch("https://playground.4geeks.com/todo/todos/" + id, {
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" }
+            })
+            obtnenerTareas()
+        } catch (error) {
+            console.log(error)
+        }
     }
+
     return (
         <div>
             <ul>
-                <input type="text"
-                    onChange={(e) => {
-                        setList(e.target.value)
-                    }}
-
+                <input 
+                    className='mb-4'
+                    style={{height: "60px", borderRadius: "5px"}}
+                    type="text"
+                    onChange={(e) => setList(e.target.value)}
                     value={list}
                     onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-
-                            setToDo([...toDo, { "label": list, "done": false }])
-                            setList(" ")
-                            console.log(toDo)
+                            if (e.key === "Enter") {
+                                setList("")
+                                guardarTarea()
+                            }
                         }
                     }
-                    }
-                    placeholder='things to do' />
-                {toDo.map((item, index) =>
+                    placeholder='Things to do' />
 
-                    <li id={index} key={index}>
+
+                {toDo.map((item, index) =>
+                    <li 
+                        id={index} 
+                        key={index}
+                        className='mb-2'
+                    >
                         {item.label}
-                        <i className="fa-solid fa-x"
-                            onClick={(event) => {
-                                { funcionDeBorrado(item.value) }
-                                console.log("hiciste click en la tarea con valor", item.value)
-                            }}>
+                        <i className="fa-solid fa-trash float-end"
+                            onClick={(e) => funcionDeBorrado(e, item.id) }>
                         </i>
                     </li>
 
                 )}
             </ul>
-            <div>{toDo.length} task</div>
+            <div>
+                <p className='' style={{color: "red"}}>
+                {toDo.length} tasks pending 
+                </p>
+                </div>
         </div>
     )
 }
